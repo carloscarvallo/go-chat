90d5008aafefd43fe20b6ee9c2d85dac95c0232d
carloscarvallo <erotomania669@gmail.com>
2016-11-16 17:59:50 -0300

    guest send message

--- lib/main.go
+++ lib/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 )
 
 const port = "8080"
@@ -16,16 +17,16 @@ func RunHost(ip string) {
 	ipAndPort := ip + ":" + port
 
 	listener, listenErr := net.Listen("tcp", ipAndPort)
-
 	if listenErr != nil {
 		log.Fatal("Error: ", listenErr)
 	}
+	fmt.Println("Listening on ", ipAndPort)
 
 	conn, acceptErr := listener.Accept()
-
 	if acceptErr != nil {
 		log.Fatal("Error: ", acceptErr)
 	}
+	fmt.Println("New connection accepted")
 
 	reader := bufio.NewReader(conn)
 	message, readErr := reader.ReadString('\n')
@@ -39,5 +40,17 @@ func RunHost(ip string) {
 // RunGuest takes destination ip
 // as argument and connects to that ip
 func RunGuest(ip string) {
+	ipAndPort := ip + ":" + port
+	conn, dialErr := net.Dial("tcp", ipAndPort)
+	if dialErr != nil {
+		log.Fatal("Error", dialErr)
+	}
+	fmt.Print("Send message: ")
 
+	reader := bufio.NewReader(os.Stdin)
+	message, readErr := reader.ReadString('\n')
+	if readErr != nil {
+		log.Fatal("Error: ", readErr)
+	}
+	fmt.Fprint(conn, message)
 }
