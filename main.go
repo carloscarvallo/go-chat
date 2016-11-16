eac743d47d4ef7ca7dc149c9efc17b037eebaf82
carloscarvallo <erotomania669@gmail.com>
2016-11-16 17:17:38 -0300

    simple receiver

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"log"
+	"net"
+	"os"
 )
 
 func main() {
@@ -13,9 +17,42 @@ func main() {
 
 	if isHost {
 		// go run main.go -listen <ip>
-		fmt.Println("is host")
+		connIP := os.Args[2]
+		runHost(connIP)
 	} else {
 		// go run main.go <ip>
-		fmt.Println("is guest")
+		connIP := os.Args[1]
+		runGuest(connIP)
 	}
+}
+
+const port = "8080"
+
+func runHost(ip string) {
+
+	ipAndPort := ip + ":" + port
+
+	listener, listenErr := net.Listen("tcp", ipAndPort)
+
+	if listenErr != nil {
+		log.Fatal("Error: ", listenErr)
+	}
+
+	conn, acceptErr := listener.Accept()
+
+	if acceptErr != nil {
+		log.Fatal("Error: ", acceptErr)
+	}
+
+	reader := bufio.NewReader(conn)
+	message, readErr := reader.ReadString('\n')
+	if readErr != nil {
+		log.Fatal("Error: ", readErr)
+	}
+
+	fmt.Println("Message received: ", message)
+}
+
+func runGuest(ip string) {
+
 }
