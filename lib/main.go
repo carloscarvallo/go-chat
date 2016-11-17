d4a25f0807c4f935851301c4eb4ae0dc903d022c
carloscarvallo <erotomania669@gmail.com>
2016-11-17 11:40:53 -0300

    for loop for receive more messages

--- lib/main.go
+++ lib/main.go
@@ -28,13 +28,26 @@ func RunHost(ip string) {
 	}
 	fmt.Println("New connection accepted")
 
+	for {
+		handleHost(conn)
+	}
+}
+
+func handleHost(conn net.Conn) {
 	reader := bufio.NewReader(conn)
 	message, readErr := reader.ReadString('\n')
 	if readErr != nil {
 		log.Fatal("Error: ", readErr)
 	}
-
 	fmt.Println("Message received: ", message)
+
+	fmt.Print("Send message: ")
+	replyReader := bufio.NewReader(os.Stdin)
+	replyMessage, replyErr := replyReader.ReadString('\n')
+	if replyErr != nil {
+		log.Fatal("Error: ", replyErr)
+	}
+	fmt.Fprint(conn, replyMessage)
 }
 
 // RunGuest takes destination ip
